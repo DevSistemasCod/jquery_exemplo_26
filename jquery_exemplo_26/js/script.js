7e0cdfe973a5c1435c74c4a679abdf6fd6a74914
DevSistemasCod <124713140+DevSistemasCod@users.noreply.github.com>
2023-10-15 15:40:52 -0300

    Update script.js

--- jquery_exemplo_26/js/script.js
+++ jquery_exemplo_26/js/script.js
@@ -1,9 +1,7 @@
 $(document).ready(function(){
-  $(document).ready(function(){
     $("button").click(function(){
       var div = $("div");  
       div.animate({left: '100px'}, "slow");
       div.animate({fontSize: '3em'}, "slow");
     });
   });
-});
