6694dcc651f21a36d8f14c576a90a1e03b979d8d
DevSistemasCod <124713140+DevSistemasCod@users.noreply.github.com>
2023-10-15 15:28:13 -0300

    Add files via upload

---
+++ jquery_exemplo_26/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $(document).ready(function(){
+    $("button").click(function(){
+      var div = $("div");  
+      div.animate({left: '100px'}, "slow");
+      div.animate({fontSize: '3em'}, "slow");
+    });
+  });
+});
